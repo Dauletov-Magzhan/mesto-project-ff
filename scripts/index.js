00ec8dc2380ff90e8e7b3b4291fea13d900e3bcc
Magzhan Dauletov <magzhan.dauletov@inbox.ru>
2023-12-01 03:41:13 +0600

    Карточки создаються и удаляются

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,34 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+function createCard(data, deleteCallback) {
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.cloneNode(true);
+    
+    cardElement.querySelector('.card__image').src = data.link;
+    cardElement.querySelector('.card__image').alt = data.name;
+    cardElement.querySelector('.card__title').textContent = data.name;
+
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+    deleteButton.addEventListener('click', function () {
+        deleteCallback(cardElement);
+    });
+
+    return cardElement;
+}
+
+function deleteCard(deleteCard) {
+    const placesItem = document.querySelector('.places__item')
+    placesItem.remove();
+}
+
+const placesList = document.querySelector('.places__list');
+
+initialCards.forEach(function (cardData) {
+    const card = createCard(cardData, function (card) {
+        deleteCard(card);
+    });
+
+    placesList.append(card);
+});
