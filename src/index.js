6b69ed13698f73b41bd07912205a4faa04734ddb
Magzhan Dauletov <magzhan.dauletov@inbox.ru>
2024-01-10 17:05:00 +0600

    Валидация форм готово

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ import './pages/index.css';
 import { initialCards } from "./scripts/cards.js";
 import { createCard, deleteCard, likeCard } from "./scripts/card.js";
 import { closeModal, openModal } from "./scripts/modal.js";
+import { clearValidation, enableValidation } from './scripts/validation.js';
 
 
 const placesList = document.querySelector('.places__list');
@@ -105,90 +106,6 @@ function openImagePopup(url, caption) {
     openModal(imagePopup);
   };
 
+//Валидация
+enableValidation();
 
-// Валидация
-const validationConfig = {
-    formSelector: '.popup__form',
-    inputSelector: '.popup__input',
-    submitButtonSelector: '.popup__button',
-    inactiveButtonClass: 'popup__button_disabled',
-    inputErrorClass: 'popup__input_type_error',
-    errorClass: 'popup__error_visible'
-  }; 
-
-
-function showInputError(formElement, inputElement, errorMessage){
-    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-    inputElement.classList.add(validationConfig.inputErrorClass);
-    errorElement.textContent = errorMessage;
-    errorElement.classList.add(validationConfig.errorClass);
-};
-
-function hideInputError(formElement, inputElement){
-    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-    inputElement.classList.remove(validationConfig.inputErrorClass);
-    errorElement.classList.remove(validationConfig.errorClass);
-    errorElement.textContent = '';
-};
-
-function checkInputValidity(formElement, inputElement){
-      if (inputElement.validity.patternMismatch) {
-        inputElement.setCustomValidity(inputElement.dataset.errorMessage);
-    } else {
-        inputElement.setCustomValidity("");
-    };
-
-    if(!inputElement.validity.valid){
-        showInputError(formElement, inputElement, inputElement.validationMessage);
-    } else {
-        hideInputError(formElement, inputElement);
-    };
-};
-
-
-function setEventListeners(formElement){
-    const inputList = Array.from(formElement.querySelectorAll(validationConfig.inputSelector));
-    const buttonElement = formElement.querySelector(validationConfig.submitButtonSelector)
-    inputList.forEach((inputElement) => {
-        inputElement.addEventListener('input', () => {
-            checkInputValidity(formElement, inputElement);
-            toggleButtonState(inputList, buttonElement)
-        });
-    });
-};
-
-function enableValidation() {
-    const formList = Array.from(document.querySelectorAll(validationConfig.formSelector));
-    formList.forEach((formElement) => {
-      formElement.addEventListener('submit', (evt) => {
-        evt.preventDefault();
-      });
-      setEventListeners(formElement);
-    });
-  };
-
-enableValidation()
-
-function hasInvalidInput(inputList){
-    return inputList.some((inputElement) => {
-      return !inputElement.validity.valid
-    })
-  }
-  
-function toggleButtonState(inputList, buttonElement){
-    if(hasInvalidInput(inputList)){
-    buttonElement.classList.add(validationConfig.inactiveButtonClass)
-    } else {
-      buttonElement.classList.remove(validationConfig.inactiveButtonClass)
-    }
-  }
-
-function clearValidation(formElement) {
-    const inputList = Array.from(formElement.querySelectorAll(validationConfig.inputSelector));
-    const buttonElement = formElement.querySelector(validationConfig.submitButtonSelector);
-
-    toggleButtonState(inputList, buttonElement);
-    inputList.forEach((inputElement) => {
-        hideInputError(formElement, inputElement)
-    });
-}
