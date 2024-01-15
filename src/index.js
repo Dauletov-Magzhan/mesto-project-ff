ad52946c6854215ee4b69bb12a4630b071fa08a4
Magzhan Dauletov <magzhan.dauletov@inbox.ru>
2024-01-15 11:31:26 +0600

    начало работы с API

--- src/index.js
+++ src/index.js
@@ -15,29 +15,31 @@ import { clearValidation, enableValidation } from './scripts/validation.js';
 
 
 const placesList = document.querySelector('.places__list');
+const displayNameElement = document.querySelector('.profile__title');
+const displayJobElement = document.querySelector('.profile__description');
+const profileImage = document.querySelector('.profile__image')
+const profileEditBtn = document.querySelector('.profile__edit-button');
+const popupTypeEdit = document.querySelector('.popup_type_edit');
+const profileAddBtn = document.querySelector('.profile__add-button');
+const popupTypeNewCard = document.querySelector('.popup_type_new-card');
+const popupCloseBtn = document.querySelectorAll('.popup__close');
+const profileForm = document.forms['edit-profile'];
+const cardForm = document.forms['new-place'];
+const nameInput = profileForm.elements.name;
+const jobInput = profileForm.elements.description;
+
 
 initialCards.forEach(function (cardData) {
     const card = createCard(cardData, deleteCard, likeCard, openImagePopup);
     placesList.append(card);
 });
 
-
-const profileEditBtn = document.querySelector('.profile__edit-button');
-const popupTypeEdit = document.querySelector('.popup_type_edit');
 profileEditBtn.addEventListener('click', function () {
     nameInput.value = displayNameElement.textContent;
     jobInput.value = displayJobElement.textContent;
-    clearValidation(profileForm)
+    clearValidation(profileForm);
     openModal(popupTypeEdit);
 });
-
-
-const profileForm = document.forms['edit-profile'];
-const nameInput = profileForm.elements.name;
-const jobInput = profileForm.elements.description;
-
-const displayNameElement = document.querySelector('.profile__title');
-const displayJobElement = document.querySelector('.profile__description');
 
 function editProfile(evt) {
     evt.preventDefault();
@@ -53,16 +55,11 @@ function editProfile(evt) {
 
 profileForm.addEventListener('submit', editProfile);
 
-
-const profileAddBtn = document.querySelector('.profile__add-button');
-const popupTypeNewCard = document.querySelector('.popup_type_new-card');
 profileAddBtn.addEventListener('click', function () {
-    clearValidation(cardForm)
+    clearValidation(cardForm);
     openModal(popupTypeNewCard);
 });
 
-
-const popupCloseBtn = document.querySelectorAll('.popup__close');
 popupCloseBtn.forEach(function (button) {
     const popupClose = button.closest('.popup');
     button.addEventListener('click', function() {
@@ -70,29 +67,23 @@ popupCloseBtn.forEach(function (button) {
     });
 });
 
-
 function addNewCard(evt) {
     evt.preventDefault();
   
     const cardNameInput = cardForm.querySelector('.popup__input_type_card-name');
-    const cardUrlInput = cardForm.querySelector('.popup__input_type_url');
-  
+    const cardUrlInput = cardForm.querySelector('.popup__input_type_url'); 
     const newCardData = {
         name: cardNameInput.value,
         link: cardUrlInput.value
     };
-  
     const newCard = createCard(newCardData, deleteCard, likeCard, openImagePopup);
-  
+
     placesList.prepend(newCard);
-  
     closeModal(popupTypeNewCard);
     cardForm.reset(); 
   };
 
-const cardForm = document.forms['new-place'];
 cardForm.addEventListener('submit', addNewCard );
-
 
 function openImagePopup(url, caption) {
     const imagePopup = document.querySelector('.popup_type_image');
@@ -109,3 +100,43 @@ function openImagePopup(url, caption) {
 //Валидация
 enableValidation();
 
+
+
+// Интеграция с API
+
+const config = {
+    baseUrl: 'https://nomoreparties.co/v1/wff-cohort-4',
+    headers: {
+      authorization: '5127d628-3fb9-4d4d-aa00-4a0279b3c5ac',
+      'Content-Type': 'application/json'
+    }
+  }
+  
+  export const getInitialCards = () => {
+      return fetch('', {})
+      // ...
+  }
+
+fetch('https://nomoreparties.co/v1/wff-cohort-4/users/me', {
+    headers: {
+        authorization: '5127d628-3fb9-4d4d-aa00-4a0279b3c5ac'
+    }
+    })
+    .then(res => res.json())
+    .then((data) => {
+    displayNameElement.textContent = data.name
+    displayJobElement.textContent = data.about
+    profileImage.src = data.avatar
+})
+    .catch((err) => console.log(err))
+
+
+fetch('https://nomoreparties.co/v1/wff-cohort-4/users/me', {
+    headers: {
+        authorization: '5127d628-3fb9-4d4d-aa00-4a0279b3c5ac'
+    }
+})
+    .then(res => res.json())
+    .then((data) => {
+        
+    })
